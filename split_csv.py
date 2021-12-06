bbde2205fe191589f878a7b707cba9c1c2b58f91
Geoffrey Gordon Ashbrook <lineality@gmail.com>
2021-12-06 10:56:59 -0500

    Update split_csv.py

--- split_csv.py
+++ split_csv.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 """
-2021.12.04
-Python script to make split .csv files into 
+Requires: pandas as pd
+
+
+2021.12.06
+This is a Python script to make split .csv files into 
 an even number of split files (2, 4, 6, etc.)
 that collectively contain all of the data.
 
 For example, if one file is too big, and you want to make
 four smaller files that contain all the data.
 
-This requires pandas to be installed (e.g. in pipenv shell).
-
-If you want the file split into 4 files,
-set the split number to 2. 
+Your select the number of files you want each file split into.
 
 This will automatically split all files in the directory
 the specified number of times.
@@ -89,24 +89,6 @@ def make_new_names(name):
         second_new_name = f'{name[:-4]}_split__002.csv'
         return first_new_name, second_new_name
 
-# flag
-file_ok = False
-
-# check to see if input number works, if not ask again
-while file_ok is False:
-
-    try:
-        # Ask how many files to split the file into?
-        number_of_final_files = int( input("Files divided into how many pieces? (even number) \n ") )
-
-        number_of_reductions = int( number_of_final_files / 2 )
-
-        file_ok = True
-
-    except:
-        file_ok is False
-        print("That did not work. Please try an even integer (like 2, 4, 6, etc.) \n")
-
 
 
 def split_csv():
@@ -118,8 +100,8 @@ def split_csv():
     # get list of csv files
     file_list = glob.glob("*.csv")
 
-    # inspection
-    print("first", file_list)
+    # # inspection
+    # print("first", file_list)
 
     ########################################################
     # reverse order of list, to avoid over-writing of files
@@ -127,14 +109,14 @@ def split_csv():
     # sort list
     file_list.sort()
 
-    # inspection
-    print("sort 1", file_list)
+    # # inspection
+    # print("sort 1", file_list)
 
     # reverse order
     file_list = file_list[::-1]
 
-    # inspection
-    print("reverse", file_list)
+    # # inspection
+    # print("reverse", file_list)
 
     # iterate through list of csv files
     for your_file_name in file_list:
@@ -198,10 +180,36 @@ def split_csv():
 
     return None
 
-# iterate and make the requested number of files:
-for i in range(0, number_of_reductions):
-    # return both df and file_counter so changes are retained
-    split_csv()
 
-# print end
-print("The End!!")
+def main_split_csv_iterator():
+    # flag
+    file_ok = False
+
+    # check to see if input number works, if not ask again
+    while file_ok is False:
+
+        try:
+            # Ask how many files to split the file into?
+            number_of_final_files = int( input("Files divided into how many pieces? (even number) \n ") )
+
+            number_of_reductions = int( number_of_final_files / 2 )
+
+            file_ok = True
+
+        except:
+            file_ok is False
+            print("That did not work. Please try an even integer (like 2, 4, 6, etc.) \n")
+
+
+
+    # iterate and make the requested number of files:
+    for i in range(0, number_of_reductions):
+        # return both df and file_counter so changes are retained
+        split_csv()
+
+    # print end
+    return print("The End!!")
+
+main_split_csv_iterator()
+
+
